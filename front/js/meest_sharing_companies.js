af5ac7a31b75b1ec822bb8263d2ecad4c70089fc
unknown <annahrishko@ukr.net>
2023-11-30 15:45:06 +0200

    edit layout

--- front/js/meest_sharing_companies.js
+++ front/js/meest_sharing_companies.js
@@ -1,44 +1,38 @@
-$('.phone').each(function(){
-    window.intlTelInput(this, {
-    utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/utils.js",
-    separateDialCode: true,
-});
-})
 $(document).ready(function(){
+    $('.phone').each(function(){
+            window.intlTelInput(this, {
+            utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/utils.js",
+            separateDialCode: true,
+        });
+    })
     $('.phone').mask('000-000-000',{placeholder:"000-000-000"}, {autoclear: true});
-});
 
-$(document).ready(function() {
     $('.select').select2();
-});
 
-$(".select").select2().change(function() {
-    $(this).valid();
-});
+    $(".select").select2().change(function() {
+        $(this).valid();
+    });
 
 
-$('#fileUpload').change(function() {
-  var i = $(this).parents('.file-upload-form').find('.add-clone-text p').clone();
-  var file = $('#fileUpload')[0].files[0].name;
-  $(this).parents('.file-upload-form').find('.add-clone-text p').text(file);
-});
+    $('#fileUpload').change(function() {
+        let i = $(this).parents('.file-upload-form').find('.add-clone-text p').clone();
+        let file = $('#fileUpload')[0].files[0].name;
+        $(this).parents('.file-upload-form').find('.add-clone-text p').text(file);
+    });
 
-var uploadField = document.getElementById("fileUpload");
+    let uploadField = document.getElementById("fileUpload");
 
-uploadField.onchange = function() {
-    if(this.files[0].size > 3000000){
-       alert("File is too big! Select file no more than 3MB");
-       this.value = "";
+    uploadField.onchange = function() {
+        if(this.files[0].size > 3000000){
+        alert("File is too big! Select file no more than 3MB");
+        this.value = "";
+        };
     };
-};
 
-$(function() {
     $.validator.addMethod("emailRegex", function(value, element) {
         return this.optional(element) || /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/i.test(value);
-    }, "");
-});
-
-$(function() {
+    }, "");       
+     
     $(".form-review").validate({
         rules: {
             "name": {
@@ -81,6 +75,6 @@ $(function() {
             },
             
         },
-    });
-});
+    });     
 
+});
